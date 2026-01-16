31740b4d120b76d242cd3f49df6edc1240784371
SUBHASIS <mukherjeebasis@gmail.com>
2026-01-16 11:36:33 +0530

    login path fix in logout function

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router-dom";
 
 export const logout = () => {
+  const navigate = useNavigate();
+
   localStorage.clear();
   sessionStorage.clear();
-  window.location.href = "/login";
+  // window.location.href = "/login";
+  navigate("/login");
 };
